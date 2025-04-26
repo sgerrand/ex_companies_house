d8f6acebc6c050e128da0f72eebbf3ab501aaade
Sasha Gerrand <sgerrand@users.noreply.github.com>
2025-04-26 10:57:45 +0100

    Install ExCoveralls for measuring code coverage

--- mix.exs
+++ mix.exs
@@ -35,8 +35,9 @@ defmodule CompaniesHouse.MixProject do
     [
       {:req, "~> 0.5.6"},
       {:bypass, "~> 2.1", only: :test},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.18", only: :test},
       {:mox, "~> 1.0", only: :test},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:expublish, "~> 2.5", only: :dev, runtime: false}
     ]
